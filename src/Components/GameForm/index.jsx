edb287c9396d61949ba10a29f05f3ae6da7165c7
Piter <piotr.podyma10@gmail.com>
2019-05-18 23:51:05 +0200

    add basic countdown clock

--- src/Components/GameForm/index.jsx
+++ src/Components/GameForm/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Questions from './../../Questions'
 import RandomAnswers from './../../Questions/RandomAnswer'
 import TakeQuestionsByNumber from './../../Questions/TakeQuestionsByNumber'
@@ -6,12 +6,37 @@ import { withRouter } from 'react-router-dom'
 import './styles.scss'
 
 class GameForm extends React.Component {
-  state = {
-    points: 0,
-    actualQuestion: 0,
-    questionNumber: 30,
-    canClick: true,
-    questions: RandomAnswers(TakeQuestionsByNumber()) || []
+  constructor() {
+    super()
+    this.state = {
+      points: 0,
+      actualQuestion: 0,
+      questionNumber: 30,
+      canClick: true,
+      questions: RandomAnswers(TakeQuestionsByNumber()) || [],
+      seconds: 5
+    }
+    this.timer = 0;
+    this.startTimer = this.startTimer.bind(this);
+    this.countDown = this.countDown.bind(this);
+  }
+
+  startTimer() {
+    if (this.timer == 0 && this.state.seconds > 0) {
+      this.timer = setInterval(this.countDown, 1000);
+    }
+  }
+
+  countDown() {
+    let restSeconds = this.state.seconds - 1;
+    this.setState({
+      seconds: restSeconds,
+    });
+    
+    if (restSeconds == 0) { 
+      clearInterval(this.timer);
+      this.next()
+    }
   }
 
   next = () => {
@@ -21,9 +46,11 @@ class GameForm extends React.Component {
     }
     this.setState({ 
       actualQuestion: this.state.actualQuestion + 1, 
-      canClick: true 
+      canClick: true,
+      seconds: 5
     })
-
+    this.timer = 0;
+    this.startTimer()
   }
 
   value = (index) => {
@@ -49,58 +76,65 @@ class GameForm extends React.Component {
   render() {
     const { questions, actualQuestion, questionNumber } = this.state
     return (
-      <div className="gameForm">
-        <div className="question">
-          <div className="questionText">{questions[actualQuestion].question}</div>
-          <div className="questionNumber">{actualQuestion} / {questionNumber}</div>
-        </div>
-        <div className="image">
-          <img 
-            src={require(`../../Questions/imgs/${questions[actualQuestion].imgId}`)} 
-            height="300" 
-            width="550" 
-          />
-        </div>
-        <div className="answers">
+      <Fragment>
+
+        <div className="gameForm">
+          <div className="question">
+            <div className="questionText">{questions[actualQuestion].question}</div>
+            <div className="questionNumber">{actualQuestion} / {questionNumber}</div>
+          </div>
+          <div className="image">
+            <img 
+              src={require(`../../Questions/imgs/${questions[actualQuestion].imgId}`)} 
+              height="300" 
+              width="550" 
+            />
+          </div>
+          <div className="answers">
+              <div 
+                className="answerA" 
+                onClick={() => this.value(0)}
+                style={this.colorBorder(0)}
+              > 
+                {questions[actualQuestion].answers[0]}
+              </div>
             <div 
-              className="answerA" 
-              onClick={() => this.value(0)}
-              style={this.colorBorder(0)}
-            > 
-              {questions[actualQuestion].answers[0]}
+              className="answerB" 
+              onClick={() => this.value(1)}
+              style={this.colorBorder(1)}
+            >
+              {questions[actualQuestion].answers[1]}
             </div>
-          <div 
-            className="answerB" 
-            onClick={() => this.value(1)}
-            style={this.colorBorder(1)}
-          >
-            {questions[actualQuestion].answers[1]}
+            <div 
+              className="answerC" 
+              onClick={() => this.value(2)}
+              style={this.colorBorder(2)}
+            >
+              {questions[actualQuestion].answers[2]}
+            </div>
+            <div 
+              className="answerD" 
+              onClick={() => this.value(3)}
+              style={this.colorBorder(3)}
+            >
+              {questions[actualQuestion].answers[3]}
+            </div>
           </div>
-          <div 
-            className="answerC" 
-            onClick={() => this.value(2)}
-            style={this.colorBorder(2)}
-          >
-            {questions[actualQuestion].answers[2]}
-          </div>
-          <div 
-            className="answerD" 
-            onClick={() => this.value(3)}
-            style={this.colorBorder(3)}
-          >
-            {questions[actualQuestion].answers[3]}
+          <div className="submit">
+            <button
+              className="submitButton"
+              onClick={this.next}
+              style={{ backgroundColor: 'green', height: '50px;'}}
+            >
+              Next please!!
+            </button>
           </div>
         </div>
-        <div className="submit">
-          <button
-            className="submitButton"
-            onClick={this.next}
-            style={{ backgroundColor: 'green', height: '50px;'}}
-          >
-            Next please!!
-          </button>
+        <div className="TIMER" style={{position: "absolute", top:"0", left: "0", backgroundColor:"green"}}>
+          <button onClick={this.startTimer}> Start </button>
+          S: {this.state.seconds}
         </div>
-      </div>
+      </Fragment>
     )
   }
 }
